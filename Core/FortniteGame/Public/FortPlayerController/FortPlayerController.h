c62e6a12d75b65c746826e30c5caf95a83f1195b
MrWallio <willwaaland@gmail.com>
2026-07-18 09:53:23 -0400

    enable builds in version 4.10

--- Core/FortniteGame/Public/FortPlayerController/FortPlayerController.h
+++ Core/FortniteGame/Public/FortPlayerController/FortPlayerController.h
@@ -216,7 +216,7 @@ public:
 		);*/
 		MH_CreateHook((LPVOID)(ImageBase + Finder::FindAFortPlayerController_RemoveInventoryItem()), RemoveInventoryItem, (LPVOID*)&RemoveInventoryItemOG);
 
-		if (Version::Fortnite_Version <= 3.6 || Version::Fortnite_Version == 1.10 || Version::Fortnite_Version == 1.11) {
+		if (Version::Fortnite_Version <= 3.6 || Version::Fortnite_Version == 1.10 || Version::Fortnite_Version == 1.11 || Version::Fortnite_Version == 4.10) {
 			HookEveryVTable(
 				AFortPlayerController::StaticClass(),
 				AFortPlayerController::StaticClass()->GetFunction("Function /Script/FortniteGame.FortPlayerController.ServerCreateBuildingActor"),
