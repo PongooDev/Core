404b76bbf762b9fcf5b6b55df5b8ac381fac6491
BeRightBack0 <131478455+BeRightBack0@users.noreply.github.com>
2026-07-31 19:14:05 +0200

    Ghost mode done i guess

--- Core/FortniteGame/Private/Player/FortPlayerControllerGameplay.cpp
+++ Core/FortniteGame/Private/Player/FortPlayerControllerGameplay.cpp
@@ -5,36 +5,115 @@
 #include "FortniteGame/Public/Items/FortWorldItem.h"
 #include "FortniteGame/Public/Items/FortItemEntry.h"
 #include "FortniteGame/Public/Player/FortPlayerControllerAthena.h"
+#include "FortniteGame/Public/Kismet/FortKismetLibrary.h"
+#include "FortniteGame/Public/Inventory/FortQuickBars.h"
+
+void AFortPlayerControllerGameplay::CheckGhostModeItemReplicated()
+{
+    static UFunction* Func = nullptr;
+
+    if (Func == nullptr)
+        Func = StaticClass()->GetFunction("Function /Script/FortniteGame.FortPlayerControllerGameplay.CheckGhostModeItemReplicated");
+
+    if (!Func) {
+        Log("FortPlayerControllerGameplay::CheckGhostModeItemReplicated: Failed to find function!");
+        return;
+    }
+
+    return GetDefaultObj()->Call<void>(Func);
+}
+
+void AFortPlayerControllerGameplay::CheckGhostModeItemRemoved(UFortWorldItemDefinition* GhostModeItemDef)
+{
+    static UFunction* Func = nullptr;
+
+    if (Func == nullptr)
+        Func = StaticClass()->GetFunction("Function /Script/FortniteGame.FortPlayerControllerGameplay.CheckGhostModeItemRemoved");
+
+    if (!Func) {
+        Log("FortPlayerControllerGameplay::CheckGhostModeItemRemoved: Failed to find function!");
+        return;
+    }
+
+    return GetDefaultObj()->Call<void>(Func, GhostModeItemDef);
+}
 
 void AFortPlayerControllerGameplay::execStartGhostMode(AFortPlayerControllerGameplay* Context, FFrame& Stack) {
-	UFortWorldItemDefinition* ItemProvidingGhostMode = nullptr;
+    UFortWorldItemDefinition* ItemProvidingGhostMode = nullptr;
+    Stack.StepCompiledIn(&ItemProvidingGhostMode);
+    Stack.IncrementCode();
 
-	Stack.StepCompiledIn(&ItemProvidingGhostMode);
-	auto Inventory = Context->WorldInventory;
-
-	// we prob need to remove the pickaxe or smth bc its removed after the og call which is kinda ahh
-	FFortItemEntry* ItemEntry = Inventory->AddItem(ItemProvidingGhostMode);
-	Inventory->Update();
-	Context->ServerExecuteInventoryItem(Context, ItemEntry->ItemGuid);
-	((AFortPlayerControllerAthena*)Context)->ClientEquipItem(ItemEntry->ItemGuid);
-
-	execStartGhostModeOG(Context, Stack);
+    Context->StartGhostMode(ItemProvidingGhostMode);
 }
 
 void AFortPlayerControllerGameplay::execEndGhostMode(AFortPlayerControllerGameplay* Context, FFrame& Stack)
 {
-	AFortInventory* Inventory = Context->WorldInventory;
-	FFortItemEntry* ItemEntry = Inventory->FindItemEntry(Context->GhostModeRepData.GhostModeItemDef, false);
+    Stack.IncrementCode();
 
-	std::cout << Context->GhostModeRepData.GhostModeItemDef->GetName().ToString().c_str() << std::endl;
-	//Inventory->RemoveItem(ItemEntry->ItemGuid, 1, true);
-	//Inventory->Update();
+    Context->EndGhostMode();
+}
 
-	execEndGhostModeOG(Context, Stack);
+void AFortPlayerControllerGameplay::StartGhostMode(UFortWorldItemDefinition* ItemProvidingGhostMode)
+{
+    if (GhostModeRepData.bInGhostMode || !ItemProvidingGhostMode)
+        return;
+
+    AFortPlayerPawnAthena* AthenaPawn = (AFortPlayerPawnAthena*)this->Pawn;
+
+    UFortKismetLibrary::K2_RemoveItemFromPlayer(this, ItemProvidingGhostMode, -1, true);
+
+    this->WorldInventory->AddItem(ItemProvidingGhostMode, 1, 0, true);
+
+    GhostModeRepData.bInGhostMode = true;
+    GhostModeRepData.GhostModeItemDef = ItemProvidingGhostMode;
+
+    AFortQuickBars* FortQuickBars = QuickBars;
+    if (!FortQuickBars) {
+        FortQuickBars = ClientQuickBars;
+    }
+    if (QuickBars)
+    {
+        if (GhostModeRepData.bInGhostMode)
+        {
+            GhostModeRepData.PreviousFocusedSlot = QuickBars->PrimaryQuickBar.CurrentFocusedSlot;
+            CheckGhostModeItemReplicated();
+        }
+        else
+        {
+            CheckGhostModeItemRemoved(GhostModeRepData.GhostModeItemDef);
+        }
+    }
+
+    OnGhostModeChanged.ProcessMulticastDelegate(&GhostModeRepData.bInGhostMode);
+
+    ForceNetUpdate();
+}
+
+void AFortPlayerControllerGameplay::EndGhostMode()
+{
+    if (UFortWorldItemDefinition* GhostModeItemDef = GhostModeRepData.GhostModeItemDef)
+        UFortKismetLibrary::K2_RemoveItemFromPlayer(this, GhostModeItemDef, -1, true);
+
+    GhostModeRepData.bInGhostMode = false;
+
+    AFortQuickBars* FortQuickBars = QuickBars;
+    if (!FortQuickBars) {
+        FortQuickBars = ClientQuickBars;
+    }
+
+    if (QuickBars) {
+        CheckGhostModeItemRemoved(GhostModeRepData.GhostModeItemDef);
+    }
+
+    OnGhostModeChanged.ProcessMulticastDelegate(&GhostModeRepData.bInGhostMode);
+
+    ForceNetUpdate();
 }
 
 void AFortPlayerControllerGameplay::Hook() {
-	ExecHook("Function /Script/FortniteGame.FortPlayerControllerGameplay.StartGhostMode", execStartGhostMode, execStartGhostModeOG);
+    ExecHook("Function /Script/FortniteGame.FortPlayerControllerGameplay.StartGhostMode", execStartGhostMode, execStartGhostModeOG);
 
-	ExecHook("Function /Script/FortniteGame.FortPlayerControllerGameplay.EndGhostMode", execEndGhostMode, execEndGhostModeOG);
+    ExecHook("Function /Script/FortniteGame.FortPlayerControllerGameplay.EndGhostMode", execEndGhostMode, execEndGhostModeOG);
+
+    Log("AFortPlayerControllerGameplay Hooked");
 }
--- Core/FortniteGame/Public/Player/FortPlayerControllerGameplay.h
+++ Core/FortniteGame/Public/Player/FortPlayerControllerGameplay.h
@@ -4,6 +4,7 @@
 #include "FortniteGame/Public/Player/FortPlayerController.h"
 #include "FortniteGame/Public/Items/Definitions/FortWorldItemDefinition.h"
 #include "FortniteGame/Public/Athena/GhostModeRepData.h"
+#include "Engine/Source/Runtime/CoreUObject/Public/UObject/ScriptDelegates.h"
 
 
 class AFortPlayerControllerGameplay : public AFortPlayerController {
@@ -11,17 +12,21 @@ public:
 	DefineUnrealClass(AFortPlayerControllerGameplay);
 
 	DefineUProperty(FGhostModeRepData, GhostModeRepData);
+
+	DefineUProperty(FMulticastScriptDelegate, OnGhostModeChanged);
 public:
 
 	static inline void (*execStartGhostModeOG)(AFortPlayerControllerGameplay* Context, FFrame& Stack);
 	static void execStartGhostMode(AFortPlayerControllerGameplay* Context, FFrame& Stack);
 
-	static inline void (*StartGhostModeOG)(AFortPlayerControllerGameplay * PlayerController, UFortWorldItemDefinition * ItemProvidingGhostMode);
-	static void StartGhostMode(AFortPlayerControllerGameplay * PlayerController, UFortWorldItemDefinition * ItemProvidingGhostMode);
-
 	static inline void (*execEndGhostModeOG)(AFortPlayerControllerGameplay* Context, FFrame& Stack);
 	static void execEndGhostMode(AFortPlayerControllerGameplay* Context, FFrame& Stack);
 
+	static void CheckGhostModeItemReplicated();
+	static void CheckGhostModeItemRemoved(UFortWorldItemDefinition* GhostModeItemDef);
+
+	void StartGhostMode(UFortWorldItemDefinition* ItemProvidingGhostMode);
+	void EndGhostMode();
 
 	static void Hook();
 };
